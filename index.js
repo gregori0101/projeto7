f9f4b89a97258a8b81b1c6f50a90393240b53f06
gregori0101 <gregorijose01@gmail.com>
2022-04-26 20:25:25 -0300

    novo

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
 const puppeteer = require('puppeteer');
-const Crawler = require('crawler');
+console.log('bem vindo ao bot conversor');
+const senha =  '270616';
+const cpf =  '00747691207';
+var rep = '1';
 
-(async () =>{
+
+
+
+(async () => {
   const browser = await puppeteer.launch({
-    //headless: false
-  });
-  const sitepesquisa = 'https://www.pneufree.com.br/vendas/passeio/195/55/15';
+      headless: false,
+      defaultViewport: null
+    });
   const page = await browser.newPage();
-  await page.goto('https://www.pneufree.com.br/cliente/login');
-  await page.waitFor('input[name="username"]');
-  await page.type('input[name="username"]','mail@mail', {delay: 100});
-  await page.type('input[name="password"]', '*********', {delay: 100});
-  await page.keyboard.press('Enter');
+  //await page.setViewport({ width: 1024, height: 800});
 
-  //Clicando no seletor
-  await page.waitFor('#wrapper > a');
-  await page.click('#wrapper > a');
+    await page.goto('https://www.vivo.com.br/para-voce');
+    await page.waitFor('a[title="São Paulo - SP"]');
+    await page.click('a[title="São Paulo - SP"]');
+    await page.waitFor(4000);
+    await page.waitFor('a[class="main-menu__login__user"]');
+    await page.click('a[class="main-menu__login__user"]');
+    await page.waitFor(9000);
+    await page.waitFor('input[class="form__input__value"]');
+    await page.type('input[class="form__input__value"]', cpf, {delay:150});
+    await page.type('input[type="password"]', senha, {delay:10});
+    await page.click('[type="submit"]');
+    await page.click('[type="submit"]');
+    await page.waitFor(15000);
+    await page.goto('https://meuvivo.vivo.com.br/content/vivo/meu-vivo/pacotes-adicionais.html');
+    await page.waitFor(10000);
 
-  //Pesquisa
-  await page.goto(sitepesquisa);
-
-  // Realiza screenshot
-  await page.screenshot({path:'pneu-195-55-15.png', fullPage:true});
-
-  const raspar = new Crawler({
-    callback : function (error,res,done){
-      if(error){
-        console.log(error);
-      }else{
-        const $ = res.$;
-        console.log($('#wrapper > div.centerdiv > div.gridProd').text().trim())
-      }
-      done();
-    }
-  });
-  raspar.queue(sitepesquisa);
-
-  await browser.close();
+    while (rep <= 25) {
+    await page.waitFor('[name="400 MB"]');
+    await page.click('[name="400 MB"]');
+    await page.waitFor(100);
+    await page.waitFor('[class="fe-txt-align-center fe-ft-weight-500 fe-indigo fe-confirmar-vivo-travel"]');
+    await page.click('[class="fe-txt-align-center fe-ft-weight-500 fe-indigo fe-confirmar-vivo-travel"]');
+    await page.waitFor('[id="purchasePackage_okButton"]');
+    await page.click('[id="purchasePackage_okButton"]');
+    await page.waitFor(100);
+    rep++;
+}
+    
+  
 })();
